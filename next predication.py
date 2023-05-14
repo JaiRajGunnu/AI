5fb0605fde4fe573d06322ea8f39e8461dce768d
Jai Raj Gunnu <78605621+JaiRajGunnu@users.noreply.github.com>
2023-05-14 23:46:19 +0530

    ARTIFICIAL INTELLIGENCE
    In this busy world, no one has time now. Next word prediction is a fundamental
    task in natural language processing, aimed at assisting users in generating
    coherent and fluent sentences. Neural networks have emerged as powerful
    models for next-word prediction, capable of capturing complex language
    patterns and dependencies. This paper presents an overview of the current state
    of next-word prediction using neural networks and explores its future scope.
    The paper emphasizes the need for ethical considerations, including fairness,
    bias mitigation, and privacy preservation in next-word prediction models.

---
+++ next predication.py
@@ -0,0 +1,84 @@
+import numpy as np
+import heapq
+import matplotlib.pyplot as plt
+from nltk.tokenize import RegexpTokenizer
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Activation
+from keras.layers import LSTM
+
+import pickle
+from keras.optimizers import RMSprop
+path = 'data.txt'
+text = open(path).read().lower()
+print('length of the corpus is: :', len(text))
+tokenizer = RegexpTokenizer(r'w+')
+words = tokenizer.tokenize(text)
+unique_words = np.unique(words)
+unique_word_index = dict((c, i) for i, c in enumerate(unique_words))
+LENGTH_WORD = 5
+next_words = []
+prev_words = []
+for j in range(len(words) - LENGTH_WORD):
+     prev_words.append(words[j:j + LENGTH_WORD])
+     next_words.append(words[j + LENGTH_WORD])
+print(prev_words[0])
+print(next_words[0])
+X = np.zeros((len(prev_words), LENGTH_WORD, len(unique_words)), dtype=bool)
+Y = np.zeros((len(next_words), len(unique_words)), dtype=bool)
+for i, each_words in enumerate(prev_words):
+   for j, each_word in enumerate(each_words):
+        X[i, j, unique_word_index[each_word]] = 1
+   Y[i, unique_word_index[next_words[i]]] = 1
+model = Sequential()
+model.add(LSTM(128, input_shape=(WORD_LENGTH, len(unique_words))))
+model.add(Dense(len(unique_words)))
+model.add(Activation('softmax'))
+optimizer = RMSprop(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+history = model.fit(X, Y, validation_split=0.05, batch_size=128, epochs=2, shuffle=True).history
+model.save('next_word_model.h5')
+pickle.dump(history, open("history.p", "wb"))
+model = load_model('next_word_model.h5')
+history = pickle.load(open("history.p", "rb"))
+plt.plot(history['acc'])
+plt.plot(history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+
+plt.plot(history['loss'])
+plt.plot(history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+def prepare_input(text):
+    x = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
+    for t, char in enumerate(text):
+       x[0, t, char_indices[char]] = 1.
+    return x
+def sample(preds, top_n=3):
+   preds = np.asarray(preds).astype('float64')
+   preds = np.log(preds)
+   exp_preds = np.exp(preds)
+   preds = exp_preds / np.sum(exp_preds)
+   return heapq.nlargest(top_n, range(len(preds)), preds.take)
+def predict_completions(text, n=3):
+
+    x = prepare_input(text)
+
+    preds = model.predict(x, verbose=0)[0]
+
+    next_indices = sample(preds, n)
+
+    return [indices_char[idx] + predict_completion(text[1:] + indices_char[idx]) for idx in next_indices]
+quotes = [    "It is not a lack of love, but a lack of friendship that makes unhappy marriages.",
+    "That which does not kill us makes us stronger.",
+    "I'm not upset that you lied to me, I'm upset that from now on I can't believe you.",
+    "And those who were seen dancing were thought to be insane by those who could not hear the music.",
+    "It is hard enough to remember my opinions, without also remembering my reasons for them!"]
+      seq = q[:40].lower()
+    print(seq)
+    print(predict_completions(seq, 5))
+    print()
